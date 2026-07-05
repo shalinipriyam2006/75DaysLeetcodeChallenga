a847f28bf9bdbbc404079210d4431d23884ba5ee
Shalini Priya M <shalinipriyam2006@gmail.com>
2026-07-05 19:39:37 +0530

    Time: 0 ms (100%), Space: 48.4 MB (31.06%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2; // Prevents potential integer overflow
+            
+            if (nums[mid] == target) {
+                return mid; // Found the target
+            } else if (nums[mid] < target) {
+                low = mid + 1; // Target is in the right half
+            } else {
+                high = mid - 1; // Target is in the left half
+            }
+        }
+        
+        return -1; // Target not found
+    }
+}
