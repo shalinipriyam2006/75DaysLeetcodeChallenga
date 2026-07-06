c9a61b3afcb64a2d9c25507f3269af5f830d996d
Shalini Priya M <shalinipriyam2006@gmail.com>
2026-07-06 21:57:53 +0530

    Time: 2 ms (99.27%), Space: 44.3 MB (75.5%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,18 +1,22 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int left = 0, right = s.length() - 1;
+        int left = 0;
+        int right = s.length() - 1;
 
         while (left < right) {
 
+            // Skip non-alphanumeric characters from the left
             while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
                 left++;
             }
 
+            // Skip non-alphanumeric characters from the right
             while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
                 right--;
             }
 
-            if (Character.toLowerCase(s.charAt(left)) != 
+            // Compare characters (case-insensitive)
+            if (Character.toLowerCase(s.charAt(left)) !=
                 Character.toLowerCase(s.charAt(right))) {
                 return false;
             }
